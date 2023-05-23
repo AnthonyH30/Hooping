b0784db8255a66ed7b42213777d9df1ef61267b6
Anthony Harisson <97698431+AnthonyH30@users.noreply.github.com>
2023-05-23 13:32:27 -0300

    Revert "[Feature] Igor/ Secao-about-me"

--- src/Pages/LandingPage/index.tsx
+++ src/Pages/LandingPage/index.tsx
@@ -1,11 +1,9 @@
-import About from "../../Components/About";
 import Header from "../../Components/Header";
 
 export default function LandingPage() {
   return (
     <>
         <Header />
-        <About />
     </>
   )
 }
